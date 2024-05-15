445fa6b5455b4c4c811d8d4f61f621c66fba078a
mcnoble1 <idowufestustemiloluwa@gmail.com>
2024-05-15 22:51:02 +0100

    feat: updated footer component :octocat:

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,11 +1,16 @@
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
 const Footer = () => {
     return (
-        <footer className="h-screen w-full pt-10 px-10 gap-[10%] bg-[#01431D] flex flex-row flex-wrap ">
+        <footer className="absolute h-screen w-full pt-10 px-10 gap-[10%] bg-[#01431D] flex flex-row flex-wrap ">
             <div className="w-2/5">
-                <h1 className="text-[#FFFED4] text-3xl">Stay in the loop with the cool things we are doing.</h1>
+                <h1 className="text-[#FFFED4] mb-5 text-3xl">Stay in the loop with the cool things we are doing.</h1>
                 <p className="text-white">Subscribe to our newsletter</p>
-                <input type="text" placeholder="johndoe@gmail.com" className="mt-5 bg-transparent p-3  rounded-full border border-[#FFFED4] outline-none" />
+                <div className="relative">
+                    <input type="text" placeholder="johndoe@gmail.com" className="mt-5 w-full text-white bg-transparent p-3 pr-12 rounded-full border border-[#FFFED4] outline-none" />
+                    <FontAwesomeIcon icon={faArrowRight} className="text-[#FFFED4] text-2xl absolute top-5 right-5 bottom-0  m-auto mr-3" />
+                </div>
             </div>
             <div className="w-2/5">
                 <div className="flex flex-row gap-[10%]">
@@ -24,9 +29,14 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div>
-                <Image src="/images/logo.png" alt="logo" className="" width={150} height={150} />
+            <div className='w-[70%]'>
+                <Image src="/images/footer-logo.svg" alt="logo" className="" width={150} height={150} />
                 <p className='text-white'>Copyright (c) 2021 | All rights reserved</p>
+            </div>
+            <div className='h-fit flex flex-row'>
+                <Image src="/images/tree.svg" alt="logo" className="absolute right-0 bottom-0" width={150} height={150} />
+                <Image src="/images/tree.svg" alt="logo" className="absolute right-50 bottom-0" width={150} height={150} />
+                <Image src="/images/tree.svg" alt="logo" className="absolute right-60 bottom-0" width={150} height={150} />
             </div>
         </footer>
     );
